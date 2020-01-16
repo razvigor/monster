fb087df1e161575bb44f7ede9fbf5c8ec1ab732f
Trkulja Sasa <kamaradsasa@gmail.com>
2020-01-16 20:45:19 +0100

    Updates

---
+++ precache-manifest.ae24663a532c9f622fd832a0747bb080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbaa078b2fe9d532e88c6f296a804ba9",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "520d024e9edfdc2e4fc6",
+    "url": "/monster/static/css/main.4d972dd7.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "520d024e9edfdc2e4fc6",
+    "url": "/monster/static/js/main.8c79cbf5.chunk.js"
+  },
+  {
+    "revision": "09b6fb0b7ef1cdca7f8f",
+    "url": "/monster/static/js/runtime-main.f3aad347.js"
+  }
+]);
